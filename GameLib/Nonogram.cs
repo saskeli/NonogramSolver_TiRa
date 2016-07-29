a965312364af4b3470e31d87b89eaa2ac4c39410
Saskeli <tuomionpupu@gmail.com>
2016-07-29 23:47:38 +0300

    Created solution structure and some classes.

---
+++ GameLib/Nonogram.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace GameLib
+{
+    public class Nonogram
+    {
+        private readonly Regex nl = new Regex(@"(?:\r(?:\n)|\n)");
+        public Nonogram(string simple)
+        {
+            foreach (string s in nl.Split(simple))
+            {
+                
+            }
+        }
+    }
+}
---
+++ SolverConsole/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GameLib;
+
+namespace SolverConsole
+{
+    class Program
+    {
+        static private readonly string simple =
+            "width 4\r\nheight 5\r\n\r\nrows\r\n4\r\n1,1\r\n1,1\r\n1,2\r\n3\r\n\r\ncolumns\r\n5\r\n1,1\r\n1,2\r\n4";
+
+        static void Main(string[] args)
+        {
+            Nonogram ng = new Nonogram(simple);
+        }
+    }
+}
