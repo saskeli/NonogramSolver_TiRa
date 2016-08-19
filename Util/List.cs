384399c048309fec11d5d9ade5fd0be1dac30936
Saskeli <tuomionpupu@gmail.com>
2016-08-19 20:55:46 +0300

    Expanden List collection and wrote correspondint tests/documentation

--- Util/List.cs
+++ Util/List.cs
@@ -9,7 +9,8 @@ namespace Util
     public class List<T>
     {
         private T[] _data;
-        private int _nexIndex;
+        private int _nextIndex;
+        private int _startIndex;
 
         /// <summary>
         /// Create a new List object with a default initial capacity.
@@ -38,27 +39,102 @@ namespace Util
         public void Add(T t)
         {
             // 0X7FEFFFFF is an internal array size limitation in c#.
-            if (_nexIndex >= 0X7FEFFFFF) throw new OverflowException("Maximum collection size exeeded");
-            if (_nexIndex >= _data.Length) Resize();
-            _data[_nexIndex] = t;
-            _nexIndex++;
+            if (_nextIndex >= 0X7FEFFFFF) throw new OverflowException("Maximum collection size exeeded");
+            if (_nextIndex >= _data.Length) Resize();
+            _data[_nextIndex] = t;
+            _nextIndex++;
         }
+
+        /// <summary>
+        /// Adds the specified element to the top of the stack.
+        /// </summary>
+        /// <param name="t">Elment to add</param>
+        public void Push(T t)
+        {
+            Add(t);
+        }
+
+        /// <summary>
+        /// Adds the specified element to the back of the Queue.
+        /// </summary>
+        /// <param name="t">Element to add</param>
+        public void Enqueue(T t)
+        {
+            Add(t);
+        }
+
+        /// <summary>
+        /// Returns and removes the element at the top of the stack
+        /// </summary>
+        /// <returns>Element at the top of the stack</returns>
+        public T Pop()
+        {
+            T t = this[-1];
+            _nextIndex--;
+            return t;
+        }
+
+        /// <summary>
+        /// Returns and removes the element at the front of the queue
+        /// </summary>
+        /// <returns>Element at the front of the queue</returns>
+        public T Dequeue()
+        {
+            T t = this[0];
+            _startIndex++;
+            return t;
+        }
+
+        /// <summary>
+        /// Gets element at the top of the stack
+        /// </summary>
+        /// <returns>Element at the top of the stack</returns>
+        public T Peek()
+        {
+            return this[-1];
+        }
+
+        /// <summary>
+        /// Gets element at the front of the queue
+        /// </summary>
+        /// <returns>Element at the front of the queue</returns>
+        public T Poll()
+        {
+            return this[0];
+        }
+
+        /// <summary>
+        /// Returns true if the collection is empty
+        /// </summary>
+        public bool IsEmpty => _startIndex == _nextIndex;
 
         /// <summary>
         /// Doubles the data capacity of the list or sets maximum allowable capacity.
         /// </summary>
         private void Resize()
         {
-            // 0X7FEFFFFF is an internal array size limitation in c#.
-            int newSize = 
-                2 * (uint)_data.Length > 0X7FEFFFFF ? 0X7FEFFFFF : _data.Length * 2;
-            newSize = newSize > 0 ? newSize : 1;
-            T[] newData = new T[newSize];
-            for (int i = 0; i < _nexIndex; i++)
+            if (_nextIndex - _startIndex <= _data.Length / 2 && _data.Length >= 4)
             {
-                newData[i] = _data[i];
+                for (int i = 0; i < _nextIndex - _startIndex; i++)
+                {
+                    _data[i] = _data[_startIndex + i];
+                }
             }
-            _data = newData;
+            else
+            {
+                // 0X7FEFFFFF is an internal array size limitation in c#.
+                int newSize =
+                    2 * (uint)_data.Length > 0X7FEFFFFF ? 0X7FEFFFFF : _data.Length * 2;
+                newSize = newSize > 0 ? newSize : 1;
+                T[] newData = new T[newSize];
+                for (int i = 0; i < _nextIndex - _startIndex; i++)
+                {
+                    newData[i] = _data[_startIndex + i];
+                }
+                _data = newData;
+            }
+            _nextIndex -= _startIndex;
+            _startIndex = 0;
         }
 
         /// <summary>
@@ -67,29 +143,31 @@ namespace Util
         /// <returns>Array of type T</returns>
         public T[] ToArray()
         {
-            T[] returnable = new T[_nexIndex];
-            for (int i = 0; i < _nexIndex; i++)
+            T[] returnable = new T[_nextIndex - _startIndex];
+            for (int i = _startIndex; i < _nextIndex; i++)
             {
                 returnable[i] = _data[i];
             }
             return returnable;
         }
 
+        /// <summary>
+        /// Get specified index in list
+        /// </summary>
+        /// <param name="index">Index of element</param>
+        /// <returns></returns>
         public T this[int index]
         {
             get
             {
-                if (index < 0 || index >= _nexIndex) throw new ArgumentOutOfRangeException();
-                return _data[index];
+                if (index < -Count || index >= _nextIndex - _startIndex) throw new ArgumentOutOfRangeException();
+                return index < 0 ? _data[_nextIndex + index] : _data[index + _startIndex];
             }
         }
 
         /// <summary>
         /// Number of elements in the List.
         /// </summary>
-        public int Count
-        {
-            get { return _nexIndex; }
-        }
+        public int Count => _nextIndex - _startIndex;
     }
 }
--- UtilTests/ListTests.cs
+++ UtilTests/ListTests.cs
@@ -24,7 +24,17 @@ namespace Util.Tests
             "Negative List capacity allowed")]
         public void ListNegativeInitTest()
         {
-            List<int> li = new List<int>(-1);
+            new List<int>(-1);
+        }
+
+        [TestMethod()]
+        public void IndexorTest()
+        {
+            List<int> li = new List<int>();
+            li.Add(3);
+            li.Add(5);
+            Assert.AreEqual(3, li[0]);
+            Assert.AreEqual(5, li[-1]);
         }
 
         [TestMethod()]
@@ -93,6 +103,62 @@ namespace Util.Tests
             li.Add(2);
             li.Add(15);
             int i = li[2];
+        }
+
+        [TestMethod()]
+        public void StackTest()
+        {
+            List<long> st = new List<long>();
+            Assert.IsTrue(st.IsEmpty, "st.IsEmpty should return true but returns false");
+            st.Push(8);
+            Assert.IsFalse(st.IsEmpty, "st.IsEmpty should return false but returns true");
+            Assert.AreEqual(1, st.Count, "st.Count should return 1 but returns " + st.Count);
+            st.Push(12);
+            Assert.IsFalse(st.IsEmpty, "st.IsEmpty should return false but returns true");
+            Assert.AreEqual(2, st.Count, "st.Count should return 1 but returns " + st.Count);
+            Assert.AreEqual(12l, st.Peek(), "st.Peek should return 12 but returns " + st.Peek());
+            long r = st.Pop();
+            Assert.AreEqual(12l, r, "st.Pop() should return 12 but returned " + r);
+            Assert.AreEqual(1, st.Count, "st.Count should return 1 but returns " + st.Count);
+            Assert.AreEqual(8l, st.Peek(), "st.Peek should return 12 but returns " + st.Peek());
+        }
+
+        [TestMethod()]
+        public void QueueResizeTest()
+        {
+            List<int> q = new List<int>(10);
+            for (int i = 0; i < 8; i++)
+            {
+                q.Enqueue(i);
+            }
+            for (int i = 0; i < 6; i++)
+            {
+                q.Dequeue();
+            }
+            for (int i = 0; i < 5; i++)
+            {
+                q.Enqueue(i);
+            }
+            Assert.AreEqual(7, q.Count);
+            Assert.AreEqual(6, q.Poll());
+        }
+
+        [TestMethod()]
+        public void QueueTest()
+        {
+            List<int> q = new List<int>();
+            for (int i = 0; i < 8; i++)
+            {
+                q.Enqueue(i);
+            }
+            Assert.AreEqual(8, q.Count);
+            Assert.IsFalse(q.IsEmpty);
+            for (int i = 0; i < 8; i++)
+            {
+                Assert.AreEqual(i, q.Poll());
+                Assert.AreEqual(i, q.Dequeue());
+            }
+            Assert.IsTrue(q.IsEmpty);
         }
     }
 }
